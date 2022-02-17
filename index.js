caae88d7cad4b6b74800b26ac89392cdd7a43d1a
Pablo Tack <tackpablo@gmail.com>
2022-02-17 12:57:40 -0800

    iss2 fetch coordination logic completed

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const request = require("request");
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!", error);
+    console.log("It didn't work (IP)!", error);
     return;
   }
 
   console.log("It worked! Returned IP:", ip);
+});
+
+fetchCoordsByIP("173.180.243.253", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work (Geolocation)!", error);
+    return;
+  }
+
+  console.log("It worked! Returned Geolocation:", coordinates);
 });
--- iss.js
+++ iss.js
@@ -24,13 +24,33 @@ const fetchMyIP = function (callback) {
       return;
     }
 
-    const data = JSON.parse(body); // parse the JSON into object
+    const ip = JSON.parse(body).ip; // parse the JSON into object
 
-    console.log(data.ip);
+    // console.log(data.ip);
 
-    if (!data.ip) callback(error, undefined);
-    else callback(null, data.ip);
+    return callback(null, ip);
   });
 };
 
-module.exports = { fetchMyIP };
+const geoUrl = "https://freegeoip.app/json/";
+
+const fetchCoordsByIP = function (ip, callback) {
+  request(`${geoUrl}${ip}`, (error, response, body) => {
+    // console.log(response.statusCode);
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) return callback(error, null);
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body); // parse the JSON into object
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
